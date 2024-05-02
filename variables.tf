20014cdcd25de1a796b6355909b536daf83a6e19
gaya38 <38839170+gaya38@users.noreply.github.com>
2024-05-02 18:35:16 +0530

    Create variables.tf
    new

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_name" {
+	type = string
+}
+variable "vpc_cidr" {
+	type = string
+}
+variable "public_subnet_name" {
+	type = string
+}
+variable "public_subnet_cidr" {
+	type = string
+}
+variable "private_subnet_name" {
+	type = string
+}
+variable "private_subnet_cidr" {
+	type = string
+}
+variable "igw_name" {
+	type = string
+}
+variable "nat_name" {
+	type = string
+}
+variable "igw_cidr" {
+	type = string
+}
+variable "region" {
+	type = "string"
+}
