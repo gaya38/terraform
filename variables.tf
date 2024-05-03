23cc4e3887a9b88e7dea1b49564692c4a8a759aa
gaya38 <38839170+gaya38@users.noreply.github.com>
2024-05-03 16:29:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -49,3 +49,61 @@ variable "vpcsecgrp" {
   description = "list of VPC security groups to associate"
   type        = list
 }
+
+ecs variables
+--------------------------------
+
+variable "ecscluster-name" {
+	type = "string"
+}
+
+variable "ecs-family" {
+	type = "string"
+}
+
+variable "networkmode" {
+	type = "string"
+}
+
+variable "region" {
+	type = "string"
+}
+
+variable "cpu" {
+  description = "Number of cpu units used by the task. If the requires_compatibilities is FARGATE this field is required."
+  type        = number
+  default     = null
+
+}
+
+variable "memory" {
+  description = "Amount (in MiB) of memory used by the task. If the requires_compatibilities is FARGATE this field is required."
+  type        = number
+  default     = null
+}
+
+variable "execution_role_arn" {
+  description = "Allows to override the default execution role used by the task. Use in combination with `create_execution_role`."
+  type        = string
+  default     = null
+}
+
+variable "task_role_arn" {
+  description = "Allows to override the default Task role used by the task. Use in combination with `create_task_role`."
+  type        = string
+  default     = null
+}
+
+variable "ecs_subnets" {
+	type = "list"
+}
+
+variable "rdsiamroles" {
+  description = "iam roles for rds to interact with other aws resources. default set null."
+  default     = []
+}
+
+variable "vpcsecgrp" {
+  description = "list of VPC security groups to associate"
+  type        = list
+}
