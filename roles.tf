0a2233334ae46d54136f3c8031c4d889a93e90ca
gaya38 <38839170+gaya38@users.noreply.github.com>
2024-05-04 18:41:27 +0530

    Create roles.tf

---
+++ roles.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_role" "ecs_execution_role" {
+  name               = "ecs_execution_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "ecs_execution_policy" {
+  name        = "ecs_execution_policy"
+  description = "Policy for ECS execution role"
+  
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ecs:CreateCluster",
+        "ecs:DeregisterContainerInstance",
+        "ecs:DiscoverPollEndpoint",
+        ...
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy_attachment" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = aws_iam_policy.ecs_execution_policy.arn
+}
